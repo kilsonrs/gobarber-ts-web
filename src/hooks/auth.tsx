d86676344139b25c6f8586e37e083d4c9e1c7cb7
Kilson <kilsonrs@gmail.com>
2020-11-18 13:24:54 -0300

    Disponibilidade no mês

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -39,8 +39,8 @@ const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token')
     const user = localStorage.getItem('@GoBarber:user')
-
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
       return { token, user: JSON.parse(user) }
     }
     return {} as AuthState;
@@ -56,6 +56,7 @@ const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem('@GoBarber:token', token)
     localStorage.setItem('@GoBarber:user', JSON.stringify(user))
 
+    api.defaults.headers.authorization = `Bearer ${token}`
     setData({ token, user })
   }, [])
 
