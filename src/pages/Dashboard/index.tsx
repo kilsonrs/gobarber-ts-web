31c3068d8cd4ccb164f76a1409db69c10398b5a0
Kilson <kilsonrs@gmail.com>
2020-11-18 10:53:27 -0300

    Calendario e estilizacoes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-
+import React, { useState, useCallback } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css'
 import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Section, Appointment, Calendar } from './styles';
 
 import logoImg from '../../assets/logo.svg'
@@ -8,6 +9,15 @@ import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if(modifiers.available) {
+
+      setSelectedDate(day)
+    }
+  },
+  [])
+
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -89,7 +99,34 @@ const Dashboard: React.FC = () => {
           </Appointment>
         </Section>
       </Schedule>
-      <Calendar />
+      <Calendar>
+        <DayPicker
+          weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+          fromMonth={new Date()}
+          disabledDays={
+            [{ daysOfWeek: [0, 6]}]
+          }
+          modifiers={{
+            available: { daysOfWeek: [1,2,3,4,5]}
+          }}
+          onDayClick={handleDayChange}
+          selectedDays={selectedDate}
+          months={[
+            'Janeiro',
+            'Fevereiro',
+            'Março',
+            'Abril',
+            'Maio',
+            'Junho',
+            'Julho',
+            'Agosto',
+            'Setembro',
+            'Outubro',
+            'Novembro',
+            'Dezembro',
+          ]}
+        />
+      </Calendar>
       </Content>
     </Container>
   )
